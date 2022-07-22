0163e3a228ffe7f3eeaff63853f243f7a375a0d6
Nick Meriano <nicholas.meriano@ephesoft.com>
2022-07-22 09:18:24 -0700

    add @typescript-eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-    extends: ['next/core-web-vitals', 'prettier'],
+    extends: ['next/core-web-vitals', 'prettier', 'plugin:@typescript-eslint/recommended'],
     rules: {
-        'no-unused-vars': 'error'
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-explicit-any': 'error'
     }
 };
